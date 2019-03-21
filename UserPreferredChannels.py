f88aa7904dfed3a7723519a7f061ed10b7346f3f
ndangelo1030 <47016396+ndangelo1030@users.noreply.github.com>
2019-03-21 19:14:57 -0400

    Create UserPreferredChannels.py

---
+++ UserPreferredChannels.py
@@ -0,0 +1,10 @@
+import user from User
+import fchannels from User
+  
+  class ViewFavorites:
+  def find(username, fchannels):
+  return username(user, fchannels)
+  
+  classAddFavorites:
+def add(fchannels, blockedchannels):
+return PreferredChannels(fchannels, blockedchannels)
