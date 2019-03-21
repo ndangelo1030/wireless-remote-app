49b6b037d7419a5af5f8a152749f4989c1912dd5
ndangelo1030 <47016396+ndangelo1030@users.noreply.github.com>
2019-03-21 19:02:14 -0400

    Update user.py

--- user.py
+++ user.py
@@ -3,12 +3,14 @@ class User:
         self.username = username
         self.password = password
 class NewUser:
-    def __init__(self, username, password, email, fname, lname):
+    def __init__(self, username, password, email, fname, lname, fchannels):
         self.username = username
         self.password = password
         self.email = email
         self.fname = fname
         self.lname = lname
+        self.fchannels = fchannels
+        
 
         
 
