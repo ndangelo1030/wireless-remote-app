f282aba8908d32f0c15c9848010baf0705ea2d1f
ndangelo1030 <47016396+ndangelo1030@users.noreply.github.com>
2019-03-21 18:48:54 -0400

    Create FamilyAccountRepo.py

---
+++ FamilyAccountRepo.py
@@ -0,0 +1,10 @@
+from user import user
+from user import GroupUser
+ 
+ class FamilyAccountRepo:
+    def find(lastname, username, password)
+    return(lastname, username, password)
+    
+    def add(familyusername, familypassword, numofusers):
+    return GroupUser(familyusername, familypassword, numofusers)
+    
