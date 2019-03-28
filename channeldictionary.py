ba1c52cf30190f8fc156645a8cb2f87d56bdd992
ndangelo1030 <47016396+ndangelo1030@users.noreply.github.com>
2019-03-28 19:29:22 -0400

    channeldictonary.py

---
+++ channeldictionary.py
@@ -0,0 +1,35 @@
+class channeldict:
+	def_init_(self):
+		self.channeldict = {
+	1:"NY1",
+	2:"CBS",
+	3:"ABC",
+	4:"NBC",
+	5:"Fox",
+	6:"6sports",
+	7:"ABCsports",
+	8:"ESPN8"
+	9:"MY9",
+	10:"TTV",
+	11:"Pix11",
+	12:"TS8",
+	13:"Y2K",
+	14:"Showtime",
+	15:"Cinemax",
+	16:"HBO",
+	17:"Yahoo Sports Network",
+	18:"Fox Sports 1",
+	19:"Opera Network",
+	20:"E News",
+	21:"PlanetEarth",
+	22:"Googletv",
+	23:"Youtubetv",
+	24:"MEtv",
+	25:"wix",
+	26:"crypto",
+	27:"Bitcointv",
+	28:"ESPN",
+	29:"ESPN2",
+	30:"SportsNow",
+	
+}
