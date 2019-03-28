e8e6bd5c310419283d7b51aff1044b62ec64a06b
tlandrycs <33505868+tlandrycs@users.noreply.github.com>
2019-03-28 18:47:43 -0400

    Update registerservice.py

--- registerservice.py
+++ registerservice.py
@@ -2,14 +2,25 @@ from user import User
 #from loginrepo import LoginRepo
 
 class RegisterService:
-    def __init__(self,repository):
+    def __init__(self,repository)
         self.repository = repository
         
     def register_user(self, username, password, email, fname, lname):
         register = self.repository.add(username, password, email, fname, lname)
         return register
 class QueryRegister:
-    def __init__(self,repository):
+    def __init__(self,repository)
         self.repository = repository
     def find(self, username, password, email)
         return RegUser(username, password, email)
+class RemoveUser:
+    def __init__(self,repository)
+        self.repository = repository
+    def remove(self,username,password,email)
+        rmuser = self.repository.remove(username,password,email)
+class CopyExisting:
+    def __init(self,repository)
+        self.repository = repository
+    def copy(self,username,password,email,fname,lname)
+        cpuser = self.repository.copy(username,password,email,fname,lname)
+    
