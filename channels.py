ad5e57ee4bcc3cb1d099346bed80a9366d03c766
ndangelo1030 <47016396+ndangelo1030@users.noreply.github.com>
2019-03-28 18:57:56 -0400

    Create channels.py

---
+++ channels.py
@@ -0,0 +1,10 @@
+class channels:
+def_init_(self,channelnumber)
+self.channelnumber = channelnumber
+	
+class Storechannels:
+	def_init_(self,channelnumber,tvshows,networks, genre)
+	self.channelnumber = channelnumber
+	self.tvshows = tvshows
+	self.networks = networks
+	self.genre = genre
